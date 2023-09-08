3ac48175ad43e36df9bf959bdc70fbcb1b14c171
Gokulg001 <gokuljune2003@gmail.com>
2023-09-08 20:38:03 +0530

    modifyed for production grade

--- index.js
+++ index.js
@@ -1,87 +1,73 @@
-const express = require('express')
-const bodyParser = require("body-parser")
-const app = express()
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json())
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 const port = process.env.PORT || 8080;
-function isletter(str){
-    return str.length === 1 && str.match(/[a-z]/i)
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+function isLetter(str) {
+    return str.length === 1 && str.match(/[a-z]/i);
 }
-function greatestalpha(array){
-    let charactercode = [];
-    let highest = []
+
+function greatestAlpha(array) {
+    let characterCode = [];
+    let highest = [];
     array.forEach(element => {
-        charactercode.push(element.charCodeAt())
-    })
-    let sortedarray = charactercode.sort()
-    console.log("Sorted array: ",sortedarray)
-    const max = sortedarray[sortedarray.length - 1]
-    console.log("Max: ",max)
-    const char = String.fromCharCode(max)
-    // console.log(char)
-    highest.push(char)
-    if(sortedarray.length > 1){
-        return highest;
-
-    }else{
-        highest = []
-        return highest
-    }
-} 
-// function largestCharacter(arr)
-// {
-
-//     let uppercase = new Array(26);
-//     uppercase.fill(false);
-//     let lowercase = new Array(26);
-//     lowercase.fill(false);
-
-//     for (let c = 0; c < arr.length; c++) {
-
-//         if (arr == arr.toLowerCase())
-//             lowercase[arr.charCodeAt() - 97] = true;
-
-//         if (arr == arr.toUpperCase())
-//             uppercase[arr.charCodeAt() - 65] = true;
-//     }
-//     for (let i = 25; i >= 0; i--) {
-//         if (uppercase[i] && lowercase[i])
-//             return String.fromCharCode(i +
-//             'A'.charCodeAt()) + "";
-//     }
-// }
-app.post('/bfhl',(req,res)=>{
-    let numbers = [];
-let alphabets = [];
-    const requestbody = req.body.data;
-    console.log("request body: ",requestbody)
-    requestbody.forEach(element => {
-        if(isletter(element)){
-            alphabets.push(element)
-        }else{
-            numbers.push(element)
+        if (isLetter(element)) {
+            characterCode.push(element.charCodeAt());
         }
-        
     });
-    console.log("Alphabets: ",alphabets)
-    console.log("Numbers: ",numbers)
-    const highest = greatestalpha(alphabets)
-    res.json({
-        "is_success": true,
-"user_id": "gokulg_20062003",
- "email" : "gg4585@srmist.edu.in",
-"roll_number":"RA2011050010019",
-"numbers": numbers,
-"alphabets": alphabets,
-"highest_alphabet": highest
-    })
+    if (characterCode.length > 0) {
+        let sortedArray = characterCode.sort();
+        const max = sortedArray[sortedArray.length - 1];
+        const char = String.fromCharCode(max);
+        highest.push(char);
+    }
+    return highest;
+}
 
-})
-app.get('/bfhl',(req,res)=>{
-    res.json({
-    operation_code:1
-    })
-})
-app.listen(port,()=>{
-    console.log('Server is running')
-})
+app.post('/bfhl', (req, res) => {
+    try {
+        if (!req.body.data || !Array.isArray(req.body.data)) {
+            res.status(400).json({ error: 'Invalid request data format' });
+            return;
+        }
+
+        let numbers = [];
+        let alphabets = [];
+
+        const requestBody = req.body.data;
+
+        requestBody.forEach(element => {
+            if (isLetter(element)) {
+                alphabets.push(element);
+            } else  {
+                numbers.push(element);
+            }
+        });
+
+        const highest = greatestAlpha(alphabets);
+
+        res.status(200).json({
+            is_success: true,
+            user_id: 'gokulg_20062003',
+            email: 'gg4585@srmist.edu.in',
+            roll_number: 'RA2011050010019',
+            numbers: numbers,
+            alphabets: alphabets,
+            highest_alphabet: highest,
+        });
+    } catch (error) {
+        console.error('Error processing request:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.get('/bfhl', (req, res) => {
+    res.status(200).json({ operation_code: 1 });
+});
+
+app.listen(port, () => {
+    console.log('Server is running on port', port);
+});
